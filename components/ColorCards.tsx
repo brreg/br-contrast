851ccb0e8b40069e3fa9bb4f06fea12f0b64d316
Øyvind Hatland <oha@brreg.no>
2023-02-22 10:13:12 +0100

    show color cards with ACPA values

--- components/ColorCards.tsx
+++ components/ColorCards.tsx
@@ -1,18 +1,30 @@
 import { brColors } from '@/data/colors'
+import { GetSettingsProps } from '@/pages';
 import styles from './ColorCards.module.css'
+import { GetContrast } from './contrast-calculator';
 
-export default function ColorCards() {
+export default function ColorCards({
+  selectedColor,
+  selectedColorIsBackground,
+  failedContrastPairIsHidden,
+  fontSize,
+  fontWeight,
+  testForWCAG2,
+}: GetSettingsProps) {
   return (
     <ul className={styles.list}>
       {
         brColors.map( colorCategory => (
-          <div key={colorCategory.name} className={styles.row}>
+          <div key={colorCategory.label} className={styles.row}>
           {
-            colorCategory.colorArray.map( color => (
-              <button key={color.value} className={styles.card} style={{ backgroundColor: color.value }}>
-                <p>{color.name}</p>
-                <p>{color.value}</p>
-              </button>
+            colorCategory.colorArray.map( cardColor => (
+              <div key={cardColor.value} className={styles.card} style={{ backgroundColor: cardColor.value }}>
+                <p>{cardColor.label}</p>
+                <p>{cardColor.value}</p>
+                <div className={styles.scorecard}>
+                  ACPA Lc-{findLcValue(cardColor.value, selectedColor, selectedColorIsBackground, fontSize, fontWeight)}
+                </div>
+              </div>
             ))
           }
           </div>
@@ -20,4 +32,12 @@ export default function ColorCards() {
       }
     </ul>
   )
+}
+
+function findLcValue(cardColor: string, selectedColor: string, selectedColorIsBackground: boolean, fontSize: number, fontWeight: number) : string {
+  if (selectedColorIsBackground) {
+    return GetContrast(cardColor, selectedColor, fontSize, fontWeight)
+  } else {
+    return GetContrast(selectedColor, cardColor, fontSize, fontWeight)
+  }
 }

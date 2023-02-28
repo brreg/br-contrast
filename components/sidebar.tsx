e5f806cafd1d4906a4d01903873b88cfa8231d6d
Øyvind Hatland <oha@brreg.no>
2023-02-28 15:44:16 +0100

    Removed header for preview text
    To avoid confusion about what text size is being tested, header is removed so only the selected font-size is show in the preview box

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -34,14 +34,12 @@ export default function Sidebar({
             <p>{backgroundColor.toUpperCase()} - Background color</p>
           </div>
           <div className={styles.preview} style={{ fontSize: fontSize, fontWeight: fontWeight, color: textColor, backgroundColor: backgroundColor}}>
-            <h1>APCA</h1>
             <p>
               The Accessible Perceptual Contrast Algorithm (APCA) is a method for calculating and predicting readability contrast. 
               APCA is a part of the larger S-Luv Accessible Color Appearance Model known as SACAM. These models are specifically 
               related to color appearance on self-illuminated RGB computer displays & devices, and also for modeling accessible user needs, 
               with a focus on readability.
             </p>
-            <h2>Lightness contrast (Lc)</h2>
             <p>
               APCA generates a lightness/darkness contrast value based on a minimum font size and color pair, and this value is perceptually based: 
               that is, regardless of how light or dark the two colors are, a contrast value of Lc 60 represents the same perceived readability contrast.

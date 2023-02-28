2e35d812ccd009c8e376189b5c7c73a4011eb4cd
Øyvind Hatland <oha@brreg.no>
2023-02-28 11:32:49 +0100

    display text and background color and scores

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,7 @@ import { SidebarProps } from '@/pages';
 import styles from '@/styles/sidebar.module.css';
 import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
+import { CalculateAPCA_value, CalculateWCAG_value, ColorData, ColorStandards, HeavyColor, LightColor } from './contrast-calculator';
 
 export default function Sidebar({
   showSidebar,
@@ -11,19 +12,26 @@ export default function Sidebar({
   textColor,
   backgroundColor
 }: SidebarProps) : JSX.Element {
+  const colorData : ColorData = {
+    textColor: textColor,
+    backgroundColor: backgroundColor,
+    fontSize: fontSize,
+    fontWeight: fontWeight
+  }
+
   return (
     <div>
       {
         showSidebar &&
         <div className={styles.sidebar}>
           <Button variant='contained' endIcon={<CloseIcon />} onClick={() => setShowSidebar(false)} />
-          <div>
-            <div />
-            <p>Text Color</p>
+          <div className={styles.textRow} >
+            <div className={styles.colorBox} style={{ backgroundColor: textColor }}/>
+            <p>{textColor.toUpperCase()} - Text Color</p>
           </div>
-          <div>
-            <div />
-            <p>Background color</p>
+          <div className={styles.textRow} >
+            <div className={styles.colorBox} style={{ backgroundColor: backgroundColor }}/>
+            <p>{backgroundColor.toUpperCase()} - Background color</p>
           </div>
           <div className={styles.preview} style={{ fontSize: fontSize, fontWeight: fontWeight, color: textColor, backgroundColor: backgroundColor}}>
             <h1>APCA</h1>
@@ -38,6 +46,15 @@ export default function Sidebar({
               APCA generates a lightness/darkness contrast value based on a minimum font size and color pair, and this value is perceptually based: 
               that is, regardless of how light or dark the two colors are, a contrast value of Lc 60 represents the same perceived readability contrast.
             </p>
+          </div>
+          <div className='scorecard' style={{ backgroundColor: LightColor(colorData, ColorStandards.APCA), color: HeavyColor(colorData, ColorStandards.APCA), borderColor: HeavyColor(colorData, ColorStandards.APCA)}}>
+            <p>APCA Lc-{CalculateAPCA_value(colorData)}</p>
+          </div>
+          <div className='scorecard' style={{ backgroundColor: LightColor(colorData, ColorStandards.WCAG_AAA), color: HeavyColor(colorData, ColorStandards.WCAG_AAA), borderColor: HeavyColor(colorData, ColorStandards.WCAG_AAA)}}>
+            <p>{CalculateWCAG_value(colorData)} AAA</p>
+          </div>
+          <div className='scorecard' style={{ backgroundColor: LightColor(colorData, ColorStandards.WCAG_AA), color: HeavyColor(colorData, ColorStandards.WCAG_AA), borderColor: HeavyColor(colorData, ColorStandards.WCAG_AA)}}>
+            <p>{CalculateWCAG_value(colorData)} AA</p>
           </div>
         </div>
       }
